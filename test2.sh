318f3b02764408b4bcac978a0631457efea1fe0f
lbaitemple <lbai@temple.edu>
2018-08-22 17:50:05 -0400

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -2,8 +2,8 @@ echo "Starting script sayIPbs "
 private=`hostname -I | sed -E -e 's/[[:blank:]]+/_/g' 
 string="private address is $private"
 res=`mosquitto_pub -h iot.eclipse.org -t raspberry/ipaddress -m $private`
-echo $string | sed 's/\./ dot /g' 
-echo $string | sed 's/\./ dot /g' | flite -voice slt
+#echo $string | sed 's/\./ dot /g' 
+#echo $string | sed 's/\./ dot /g' | flite -voice slt
 
 # echo $string | flite -voice slt 
 #`flite -voice slt -t $string`
