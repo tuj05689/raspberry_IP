413979fb6041af409d4b7ea50831161c570ce765
lbaitemple <lbai@temple.edu>
2019-01-15 15:46:50 -0500

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 
-say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "
-http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
+#say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "
+#http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
 
 
 echo "Starting script sayIPbs "
@@ -11,6 +11,6 @@ string="private address is $private"
 res=`mosquitto_pub -h iot.eclipse.org -t raspberry/ipaddress -m $private`
 #echo $string | sed 's/\./ dot /g' 
 #echo $string | sed 's/\./ dot /g' | flite -voice slt
-say $string 
+#say $string 
 # echo $string | flite -voice slt 
 #`flite -voice slt -t $string`
