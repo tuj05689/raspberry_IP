1d569209621a175a1521b2e6cb48268c6d02b264
Adam P <49652081+tuj05689@users.noreply.github.com>
2019-10-01 19:16:08 -0400

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -9,7 +9,7 @@ echo "Starting script sayIPbs "
 private=`hostname -I | sed -E -e 's/[[:blank:]]+/_/g' `
 #ip=$(wget https://raw.githubusercontent.com/lbaitemple/raspberry_IP/master/ipaddress  -q -O -)
 #string="private address is $private"
-res=`mosquitto_pub -h m16.cloudmqtt.com -p 12247 -u pspniyjc -P sBm4EpaDgRe5 -t raspberry/ipaddress -m $private`
+res=`mosquitto_pub -h soldier.cloudmqtt.com -p 14690 -u crtxxhrb -P gZRGAEKYYTKa -t raspberry/ipaddress -m $private`
 #echo $string | sed 's/\./ dot /g' 
 #echo $string | sed 's/\./ dot /g' | flite -voice slt
 #say $string 
